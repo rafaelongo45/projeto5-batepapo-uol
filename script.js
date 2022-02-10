25177825dd2675dac1b3c96157baffaf2233198b
Rafael Longo <rafaelongo45@gmail.com>
2022-02-10 02:24:30 -0300

    Mensagens estão sendo atualizadas a cada 3s. Usuário consegue entrar na sala e está sendo checado se ele está presente a cada 5s

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-function pegaMensagemServidor(){
-    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+let dado = null;
+const janelaChat = document.querySelector('main');
+let nomeUsuario = null;
+let ultimaMensagem = null;
+let ultimaMensagemHTML = null;
+
+function entrarNoSite(){
+    nomeUsuario = prompt('Qual é o seu nome?');
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: nomeUsuario})
+    requisicao.catch(falhaNaEntrada);
+}
+
+function falhaNaEntrada(){
+    alert("Digite outro nome. Este já está em uso!")
+    entrarNoSite();
+}
+
+function checarPresencaUsuario(){
+    axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: nomeUsuario})
+}
+
+function pegaMensagemServidor() {
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
     promessa.then(pegaDadosMensagens);
 }
 
-function pegaDadosMensagens(objeto){
-    const dado = objeto.data;
-    console.log(objeto.data) //tirar depois
-    console.log(objeto.data[0].type); //tirar depois
-    setInterval(separaMensagemPorTipo(dado), 3000);
+function pegaDadosMensagens(objeto) {
+    dado = objeto.data;
+    console.log(objeto.data)
+    objetoUltimaMensagem = dado[dado.length-1].time;
+    renderizaMensagem();
 }
 
-function separaMensagemPorTipo(dado){
+function renderizaMensagem() {
     let tipoMensagem = null;
-    for(let i = 0; i < dado.length; i++){
-        renderizaMensagem(dado, tipoMensagem, i);
+    janelaChat.innerHTML = null;
+
+    for (let i = 0; i < dado.length; i++) {
+        if (dado[i].type === 'status') {
+            tipoMensagem = 'status-sala';
+            janelaChat.innerHTML += `<div class = 'mensagem ${tipoMensagem}'>
+            <span class = "hora">(${dado[i].time}) </span>  <span class = "usuario">${dado[i].from}</span> ${dado[i].text}
+            </div>`;
+        } else if (dado[i].type === 'private_message') {
+            tipoMensagem = 'reservada';
+            janelaChat.innerHTML += `<div class = 'mensagem reservada'> 
+            <span class = "hora">(${dado[i].time})</span>  <span class = "usuario">${dado[i].from}</span>  reservadamente para <span class = "usuario"> ${dado[i].to}</span>: Oi gatinha quer tc? 
+            </div>`;
+        } else {
+            tipoMensagem = "";
+            janelaChat.innerHTML += `<div class = 'mensagem ${tipoMensagem}'>
+            <span class = "hora">(${dado[i].time}) </span>  <span class = "usuario">${dado[i].from}</span> para <span class = "usuario"> ${dado[i].to}: </span>${dado[i].text}
+            </div>`;
+        }
+        pegaDadoUltimaMensagem(i)
+    }
+
+}
+
+function pegaDadoUltimaMensagem(i){
+    if (i === dado.length - 1){
+        ultimaMensagem = document.querySelectorAll('.mensagem') 
+        ultimaMensagemHTML = ultimaMensagem[i].innerHTML;
     }
 }
 
-function renderizaMensagem(dado, tipoMensagem, i){
-    const janelaChat = document.querySelector('main');
-    if (dado[i].type === 'status'){
-        tipoMensagem = 'status-sala';
-        janelaChat.innerHTML += `<div class = 'mensagem ${tipoMensagem}'>
-        <span class = "hora">(${dado[i].time}) </span>  <span class = "usuario">${dado[i].from}</span> ${dado[i].text}
-        </div>`;
-    }else if (dado[i].type === 'private'){
-        tipoMensagem = 'reservada';
-        janelaChat.innerHTML += `<div class = 'mensagem reservada'> 
-        <span class = "hora">(${dado[i].time})</span>  <span class = "usuario">${dado[i].from}</span>  reservadamente para <span class = "usuario"> ${dado[i].to}: Oi gatinha quer tc? 
-        </div>`;
-    }else{
-        tipoMensagem = "";
-        janelaChat.innerHTML += `<div class = 'mensagem ${tipoMensagem}'>
-        <span class = "hora">(${dado[i].time}) </span>  <span class = "usuario">${dado[i].from}</span> para <span class = "usuario"> ${dado[i].to}: </span>${dado[i].text}
-        </div>`;
-    }
+function atualizaChat() {
+    pegaMensagemServidor();
+    ultimaMensagem[dado.length-1].scrollIntoView();
 }
 
+entrarNoSite();
+setInterval(checarPresencaUsuario, 5000);
 pegaMensagemServidor();
+setInterval(atualizaChat, 3000)
